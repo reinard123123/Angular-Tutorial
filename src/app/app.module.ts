d2292f7d7aa0ee024209a1e676e89cbc15679c2d
reinardbornillo15 <reinardbornillo15@gmail.com>
2024-04-02 22:20:52 +0800

    Angular Tutorial

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { WishlistComponent } from './wish-list/wish-list.component';
+import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
+import { WishFilterComponent } from './wish-filter/wish-filter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WishlistComponent,
+    AddWishFormComponent,
+    WishFilterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
